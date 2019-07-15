83e7350e8a6ce19b7b25aa9767e7c7134ecaf91e
Tomoya Shibazaki <tshibazaki8165@gmail.com>
2019-07-15 15:30:34 +0900

    make it better

--- crud/settings/prod.py
+++ crud/settings/prod.py
@@ -148,7 +148,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 # STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
-# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 
